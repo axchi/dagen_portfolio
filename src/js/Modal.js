822ceca3a27e59f2913b92abe9f278af28adc710
Diana Mkrtchyan <dianamukuch@gmail.com>
2018-10-12 20:36:51 -0500

    organize the files

---
+++ src/js/Modal.js
@@ -0,0 +1,15 @@
+
+const modal = function(){
+    let modal = document.querySelector(".js-modal--content");
+    let trigger = document.querySelector('.js-modal--trigger');
+    let closeButton = document.querySelector('.js-modal--close');
+
+    function toggleModal() {
+        modal.classList.toggle("--show");
+    }
+
+    trigger.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+}
+
+exports.modal = modal;
